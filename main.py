6faa86cc6e0d6830e2d75fb3986688b39fd19a3a
jayantdahiya <jayantdahiya1999@gmail.com>
2022-03-13 07:35:49 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from email.policy import default
+from pickle import GLOBAL
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import sklearn.model_selection as model_selection
+from datetime import date
+from fastapi import FastAPI
+
+
+
+
+def stock_prediction(stock_name):
+    date_today = date.today()
+
+    data = yf.download(stock_name, start = "2013-10-10", end=date_today)
+    df = pd.DataFrame(data)
+    x = df.loc[:,'High':'Volume']
+    y = df.loc[:,'Open']
+
+    x_train,x_test,y_train,y_test = model_selection.train_test_split(x,y,test_size= 0.2,random_state = 0)
+    LR = LinearRegression()
+    LR.fit(x_train,y_train)
+    LR.score(x_test,y_test)
+
+    x_test = pd.DataFrame(x)
+    ticker = yf.Ticker(stock_name)
+    stock_today = ticker.history(period="1d")
+    today = pd.DataFrame(stock_today["Open"])
+
+    test_data = x_test.iloc[-1:]
+    prediction = LR.predict(test_data).round(2)
+
+    return (prediction)
+
+
+
+
+# print(stock_prediction(stock_name))
+
+
+app = FastAPI()
+
+@app.get('/')
+def prediction(input_name):
+    pred = stock_prediction(input_name)
+    a = int(pred)
+    return (a)
