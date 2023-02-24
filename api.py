65f4494f0afdb77bcba8b8ce21b75d8ae16edee6
jayantdahiya <jayantdahiya1999@gmail.com>
2023-02-24 17:42:02 +0000

    added vercel.json

---
+++ api.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from Models.Prophet import pred as Prophet_Forecast
+
+app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "https://5173-jayantdahiy-stockpredic-fmaagmgmu8d.ws-us87.gitpod.io"
+]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"]
+    )
+
+
+class StockIn(BaseModel):
+    ticker: str
+
+
+@app.get('/')
+def index():
+    return {'Stock Predictor API'}
+
+# This is the route that is called when the user wants the Prophet model
+@app.get('/Prophet')
+def Prophet(ticker: str):
+    return Prophet_Forecast(ticker)
--- main.py
+++ main.py
@@ -1,38 +1,4 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+import uvicorn
 
-from Models.Prophet import pred as Prophet_Forecast
-
-app = FastAPI()
-
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:8000",
-    "*"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-class StockIn(BaseModel):
-    ticker: str
-
-
-@app.get('/')
-def index():
-    return {'Stock Predictor API'}
-
-# This is the route that is called when the user wants the Prophet model
-@app.get('/Prophet')
-def Prophet(ticker: str):
-    return Prophet_Forecast(ticker)
+if __name__ == "__main__":
+  uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
