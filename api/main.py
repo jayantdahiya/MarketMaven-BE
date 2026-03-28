c8223afe65d04f7f2480c8320a7b51020fdf70e9
jayantdahiya <jayantdahiyadev@gmail.com>
2026-03-28 19:29:21 +0530

    fix: update server port configuration to use environment variable
    - Modified the main.py file to retrieve the server port from an environment variable, defaulting to 8000 if not set. This change enhances flexibility for deployment configurations.

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
+import os
+
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run('api.app:app', host='0.0.0.0', port=9000, reload=True)
+    port = int(os.environ.get('PORT', '8000'))
+    uvicorn.run('api.app:app', host='0.0.0.0', port=port, reload=True)
